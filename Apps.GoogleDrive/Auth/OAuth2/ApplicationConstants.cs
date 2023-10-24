5f80e23b962308054c7bf6d0cdee8db5c04ba3c8
Ihor Kriatov <ihor.kriatov@blackbird.io>
2023-10-24 19:27:09 +0300

    code

---
+++ Apps.GoogleDrive/Auth/OAuth2/ApplicationConstants.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.GoogleDrive
+{
+    public class ApplicationConstants
+    {
+        public const string ClientId = "#{GOOGLEDRIVE_CLIENT_ID}#";
+        public const string ClientSecret = "#{GOOGLEDRIVE_CLIENT_SECRET}#";
+        public const string Scope = "#{GOOGLEDRIVE_SCOPE}#";
+    }
+}
